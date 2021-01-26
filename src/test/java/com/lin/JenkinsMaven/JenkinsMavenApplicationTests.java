97d767b285b3d659d427884a6f22a06c86158a7e
Md Hasan <hasan.mmd28@gmail.com>
2021-01-25 23:03:58 -0500

    first commit

---
+++ src/test/java/com/lin/JenkinsMaven/JenkinsMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lin.JenkinsMaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
